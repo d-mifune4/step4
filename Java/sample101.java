880274c373611d50557a606560cbca7c63bba14f
d-mifune4 <d.mifune@shiny-pork-works.co.jp>
2026-04-07 13:45:36 +0900

    Delete Java directory

--- Java/sample101.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class sample101 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello,world");
-    }
-}
--- Java/sample103.java
+++
@@ -1,9 +0,0 @@
-package Java;
-
-public class sample103 {
-
-    public static void main(String[] args){
-        System.out.println("ABCDEDF");
-    }
-    
-}
